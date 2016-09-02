0241f240d300556dcf57058b7a503aec9c96171c
jfbookingbug <jfenton@bookingbug.com>
2016-09-02 10:18:58 +0100

    Update bb_spec.rb

--- spec/features/bb_spec.rb
+++ spec/features/bb_spec.rb
@@ -2,68 +2,19 @@ require "spec_helper"
 require "capybara"
 require "cucumber"
 
-describe "BookingBug's admin interface", :sauce => true do
-  it "Should successfully load the angular booking journey" do
-  visit 'https://anz-staging.bookingbug.com/'
-
-  expect(page).to have_content('BookingBug')
-
-  fill_in('login_email', with: 'qa@bookingbug.com')
-	 fill_in('login_password', with: 'qwaszx12')
-
-	click_button('Login')
-	expect(page).to have_content('Current password')
-		fill_in('login_current_password', with: 'qwaszx12')
-	click_button('Change my password')
-	 expect(page).to have_content('Jobs')
-
-  find_link('Logins').click
-
-  expect(page).to have_content('Registered')
-	 find_link('Search').click
-
-	fill_in('as_admin__login-search-input', with: 'suzanne.dunn@anz.com')
-	click_button('Search')
-	
-
-
-  expect(page).to have_content('1 Found')
-	 page.should_not have_content('16')
-
-  find('a.show_actions').click
-visit('https://anz-staging.bookingbug.com/admin/user/show?id=12&eid=65409b0d65b64321dbb22b3b4f4f24fb')
-
- expect(page).to have_content('Login as User', wait: 20)
-  find_link('Login as User').click
-
-  visit 'https://anz-staging.bookingbug.com/login/login?select=business'
-    fill_in('limit', with: 'Bondi')
-
-
-  find_link('Bondi Junction Westfield').click
-
-  visit('https://anz-staging.bookingbug.com/view/dashboard/add_booking_frame?add_booking_menu=anz&base=anz&company_id=37017&iarray=128&jd=2457595')
-
-  expect(page).to have_content('Make a Booking')
-
-select('0', from: 'service',  match: :first)	
-
-  find_button('Book').click
-
-
-	
-  expect(page).to have_content('Select a time')
-  
-
-  
-  
- 
-expect(page).to have_css('span.period-label.ng-binding')
-find('div.times', match: :first).click
-
-
-puts 'calendar has loaded!!'
-
-    
+describe "RBC BB Customer journey widget Cross Browser", :sauce => true do
+	it "Should successfully complete E2E test of current functionality" do
+# Visits the RBC staging widget with a pre defined URL
+	visit 'http://bespoke.bookingbug.com/rbc/staging/new_customer.html?firstName=Test&lastName=Automation&mobile=5815000800'
+	expect(page).to have_content 'How would you like to connect'
+	find('.text-center.service-item', :text => arg1).click
+	sleep 2
+       	find_button('Confirm').click
+	find('i.rbc-icon-arrow-down.avail-text', match: :first).click
+        find('li.time-slot.ng-scope', match: :first).click   
+        find_button('Continue').click  
+        find_button('Confirm').click
+        find('#loading_icon')
+# End of current functionality for now.        
   end
 end
