5a3c7dc3c733ffd35ce17999fb9a51e6ce7f0b27
jfbookingbug <jfenton@bookingbug.com>
2016-09-05 15:02:38 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ Capybara.register_driver :selenium do | app|
     :version => ENV['version'],
     :browserName => ENV['browserName'],
     :platform => ENV['platform'],
-    :maxInstances: => 1
+    :maxInstances => 1
     
   }
 
