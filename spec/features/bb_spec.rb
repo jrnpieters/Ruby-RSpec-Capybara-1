fb2ebab6e03e258def7178068ff1dce088cf55f2
jrnpieters <jpieters@bookingbug.com>
2016-09-29 15:19:46 +0100

    efew

--- spec/features/bb_spec.rb
+++ spec/features/bb_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 require "capybara"
 require "cucumber"
 
-describe "RBC BB Customer journey widget Cross Browser", :sauce => true do
-	it "Should successfully complete E2E test of current functionality" do
-# Visits the RBC staging widget with a pre defined URL
+describe "BookingBug Automation BootCamp", :sauce => true do
+	it "Should run inside of SauceRunner and test in da cloud!!!" do
+# Visits our widget
 	visit 'http://bespoke.bookingbug.com/rbc/staging/new_customer.html?firstName=Test&lastName=Automation&mobile=5815000800'
 	expect(page).to have_content 'How would you like to connect'
 	find('.text-center.service-item', :text => 'Video Conference').click
